6e7b6100879f43c327b2598bd4335f9c8f012a20
Alexander Ershov <eershoov@gmail.com>
2019-11-30 10:53:06 +0300

    Initial Commit

---
+++ HotelsTestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelsTestTask
+//
+//  Created by Alexander Ershov on 30.11.2019.
+//  Copyright © 2019 Alexander Ershov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
