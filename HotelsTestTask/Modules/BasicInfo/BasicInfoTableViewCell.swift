b46ad70880517f787829fafc764bc15913a3ea2b
Alexander Ershov <eershoov@gmail.com>
2019-12-21 13:30:45 +0300

    Added view and view model for basic info table view cell

--- HotelsTestTask/Modules/BasicInfo/BasicInfoTableViewCell.swift
+++ HotelsTestTask/Modules/BasicInfo/BasicInfoTableViewCell.swift
@@ -8,19 +8,63 @@
 
 import UIKit
 
-class BasicInfoTableViewCell: UITableViewCell {
+
+/// View protocol for `BasicInfo` table view cell
+protocol BasicInfoCellViewProtocol: class {
+    
+    /// Configures table view cell
+    /// - Parameter viewModel: data for configuration
+    func configure(with viewModel: BasicInfoCellViewModelProtocol)
+}
+
+/// View model protocol for `BasicInfo` table view cell
+protocol BasicInfoCellViewModelProtocol {
+    
+    /// Text for number of start label
+    var numberOfStars: String { get }
+    
+    /// Text for hotel name label
+    var hotelName: String { get }
+    
+    /// Text for number of available suits label
+    var numberOfSuitsAvailable: String { get }
+    
+    /// Text for hotel address label
+    var hotelAddress: String { get }
+    
+    /// Text for distance to the centre label
+    var distanceToTheCentre: String { get }
+}
+
+class BasicInfoTableViewCell: UITableViewCell, BasicInfoCellViewProtocol {
     @IBOutlet private weak var numberOfStarsLabel: UILabel!
     @IBOutlet private weak var hotelNameLabel: UILabel!
     @IBOutlet private weak var numberOfSuitsAvailableLabel: UILabel!
     @IBOutlet private weak var hotelAddressLabel: UILabel!
     @IBOutlet private weak var distanceToTheCentreLabel: UILabel!
 
-    func fillUI(with hotelInfo: BasicHotelInfoProtocol) {
-        numberOfStarsLabel.text = String.init(repeating: "⭐️", count: Int(hotelInfo.stars))
-        hotelNameLabel.text = hotelInfo.name
-        numberOfSuitsAvailableLabel.text = "🛏 \(hotelInfo.suitesAvailability.count) suits available"
-        hotelAddressLabel.text = hotelInfo.address
-        distanceToTheCentreLabel.text = "🏃‍️ Distance to the centre: \(hotelInfo.distance)"
+    func configure(with viewModel: BasicInfoCellViewModelProtocol) {
+        numberOfStarsLabel.text = viewModel.numberOfStars
+        hotelNameLabel.text = viewModel.hotelName
+        numberOfSuitsAvailableLabel.text = viewModel.numberOfSuitsAvailable
+        hotelAddressLabel.text = viewModel.hotelAddress
+        distanceToTheCentreLabel.text = viewModel.distanceToTheCentre
+    }
+}
+
+struct BasicInfoCellViewModel: BasicInfoCellViewModelProtocol {
+    let numberOfStars: String
+    let hotelName: String
+    let numberOfSuitsAvailable: String
+    let hotelAddress: String
+    let distanceToTheCentre: String
+    
+    init(hotelInfo: BasicHotelInfoProtocol) {
+        numberOfStars = String(repeating: "⭐️", count: Int(hotelInfo.stars))
+        hotelName = hotelInfo.name
+        numberOfSuitsAvailable = "🛏 \(hotelInfo.suitesAvailability.count) suits available"
+        hotelAddress = hotelInfo.address
+        distanceToTheCentre = "🏃‍️ Distance to the centre: \(hotelInfo.distance)"
     }
 }
 
