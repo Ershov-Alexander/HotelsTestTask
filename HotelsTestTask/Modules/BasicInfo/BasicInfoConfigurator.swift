ab4f04e5fde89491a90d54693f4ad9ae146fce05
Alexander Ershov <eershoov@gmail.com>
2019-12-21 13:32:46 +0300

    added basic info configurator

---
+++ HotelsTestTask/Modules/BasicInfo/BasicInfoConfigurator.swift
@@ -0,0 +1,28 @@
+//
+//  BasicInfoConfigurator.swift
+//  HotelsTestTask
+//
+//  Created by Alexander Ershov on 19.12.2019.
+//  Copyright © 2019 Alexander Ershov. All rights reserved.
+//
+
+import Foundation
+
+
+/// Configures VIPER parts for `BasicInfo` module
+protocol BasicInfoConfiguratorProtocol: class {
+    func configure(with viewController: BasicInfoViewController)
+}
+
+class BasicInfoConfigurator: BasicInfoConfiguratorProtocol {
+    func configure(with viewController: BasicInfoViewController) {
+        let presenter = BasicInfoPresenter(view: viewController)
+        let interactor = BasicInfoInteractor(delegate: presenter)
+        let router = BasicInfoRouter(viewController: viewController)
+        
+        viewController.presenter = presenter
+        presenter.interactor = interactor
+        presenter.router = router
+    }
+}
+
